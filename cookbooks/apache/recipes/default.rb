3ea19c22db867b3f93c895b56e51866a2e6c3e01
slufoot80 <slufoot80@yahoo.com>
2017-10-21 18:08:01 +0000

    added motd and apache October-21-2017-18:08:01

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
-package 'apache2' do
+if node['platform_family'] == "rhel"
   package_name 'httpd'
+elsif node['platform_family'] == "debian"
+  package = "apache2"
+end
+  
+package 'apache2' do
+  package_name package
   action :install
 end
 
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,3 +2,5 @@ file 'default www' do
   path '/var/www/html/index.html'
   content 'Hello World!'
 end
+
+include_recipe 'apache::websites'
